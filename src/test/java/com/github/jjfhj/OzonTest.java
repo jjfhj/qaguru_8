18c363857a85c13680d70d7f64ab751570df4397
Karina <aiwpront@gmail.com>
2021-11-16 22:36:57 +0400

    added ValueSource

--- src/test/java/com/github/jjfhj/OzonTest.java
+++ src/test/java/com/github/jjfhj/OzonTest.java
@@ -4,11 +4,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class OzonTest extends TestBase {
+
+    @ValueSource(strings = {"Всё о Муми-троллях. Книга 1", "Всё о Муми-троллях. Книга 2"})
+    @DisplayName("Отображение результатов поиска")
+    @Tag("Blocker")
+    @Tag("High")
+    @ParameterizedTest(name = "Отображение товара {0} в результатах поиска")
+    void checkingBookInSearchResults(String searchQuery) {
+        open("https://www.ozon.ru/");
+        $("[name='text']").setValue("Все о Муми-троллях").submit();
+        $(".a9x3").shouldHave(text(searchQuery));
+    }
 
     @CsvSource({
             "Мягкая обложка, Обложка: Мягкая обложка",
