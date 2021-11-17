3f54a0a4e6cdb2ca7731113cdae3d13e581f7fad
Karina <aiwpront@gmail.com>
2021-11-17 16:06:49 +0400

    added CatalogItems

---
+++ src/test/java/com/github/jjfhj/CatalogItems.java
@@ -0,0 +1,16 @@
+package com.github.jjfhj;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CatalogItems {
+
+    public static Stream<Arguments> productCategories() {
+        return Stream.of(
+                Arguments.of("Умный дом и безопасность", List.of("Охранные системы, Видеонаблюдение, Умный дом")),
+                Arguments.of("Моноблоки и системные блоки", List.of("Системные блоки, Моноблоки"))
+        );
+    }
+}
